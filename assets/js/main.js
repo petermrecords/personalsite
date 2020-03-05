64631296ba32034e84a6cf8fd88c8ceff8dcb032
BeggarsPeterM <petermenniti@beggars.com>
2020-03-05 17:15:01 -0500

    mostly finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,36 @@
 function toggleResumeDetails(element) {
-  var resumeBody = $(element).parent().parent().parent()
-  console.log(resumeBody);
-  // resumeBody.slideToggle(500);
+  var resumeBody = $(element).parent().find("li.resume-details");
+  resumeBody.slideToggle(500);
 };
+
+function showResumeShadow(element) {
+  var resumeHeadline = $(element).find("div.resume-content");
+  var resumeIcon = $(element).find("i");
+  resumeHeadline.css("text-shadow", "0em 0em 0.05em #BE4141");
+  resumeIcon.css("text-shadow","0px 0px 2px #41BE41");
+};
+
+function hideResumeShadow(element) {
+  var resumeHeadline = $(element).find("div.resume-content");
+  var resumeIcon = $(element).find("i");
+  resumeHeadline.css("text-shadow", "none");
+  resumeIcon.css("text-shadow","none");
+}
 
 $(document).ready(function() {
 
   // show/hide resume details
-  $(".resume-headline i").click(function(e) {
+  $(".resume-headline").click(function(e) {
     toggleResumeDetails(this);
+  });
+
+  // headline hover styling
+  $(".resume-headline").mouseenter(function(e) {
+    showResumeShadow(this);
+  });
+
+  $(".resume-headline").mouseleave(function(e) {
+    hideResumeShadow(this);
   });
 
   // download button
